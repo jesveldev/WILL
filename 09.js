edd64575454e10128d292d3744137724bc3255f0
Jesús Velásquez <rgb2black@gmail.com>
2022-12-11 14:09:17 -0400

    Ejercicios del Simulation Challenge realizados

--- 09.js
+++ 09.js
@@ -25,6 +25,18 @@ function filtrar(funcion) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
 
+  Array.prototype.filtrar = function(cb){
+
+    let nuevoArreglo = [];
+
+    for (let i in this){
+      if(cb(this[i],i)){
+        nuevoArreglo.push(this[i]);
+      }
+    }
+
+    return nuevoArreglo;
+  }
 };
 
 // No modifiques nada debajo de esta linea //
